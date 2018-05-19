651864e8e591d955b27ee8a4f056d65c1b02890d
Zalan Farkas <zalan.farkas96@gmail.com>
2018-05-19 18:31:02 +0100

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,26 @@
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    //let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+
+    thumbnails.forEach(function(thumbnail){
+        thumbnail.addEventListener("click", function(){
+            // Set clicked image as main image.
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            mainImage.setAttribute("src", newImageSrc);
+            
+            // Change which image is current.
+            let currentClass = "current";
+            document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+            thumbnail.parentNode.classList.add(currentClass);
+
+            // Update image info.
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
+            title.innerHTML = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
+        });
+    });
   }
   
